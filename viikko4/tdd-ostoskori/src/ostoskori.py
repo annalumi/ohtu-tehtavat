28707d038dc278ccb0d219c30e1676369431792d
Veera0742 <veera.lumioksa@gmail.com>
2022-03-24 09:34:58 +0200

    lisätty testi hinnan_löytyminen

--- viikko4/tdd-ostoskori/src/ostoskori.py
+++ viikko4/tdd-ostoskori/src/ostoskori.py
@@ -14,7 +14,9 @@ class Ostoskori:
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        return 0
+        hinnat = map(lambda t: t._hinta, self._ostokset)
+
+        return sum(hinnat)
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
