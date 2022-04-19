90f2c6926a5a672ff76e080e58a1230956eb695e
Veera0742 <veera.lumioksa@gmail.com>
2022-04-19 13:03:27 +0300

    Tehtävä 6.5

--- viikko6/query-language/src/query_builder.py
+++ viikko6/query-language/src/query_builder.py
@@ -15,3 +15,6 @@ class QueryBuilder:
 
     def hasFewerThan(self, value, attr):
         return QueryBuilder(And(self._matcher, HasFewerThan(value, attr)))
+
+    def oneOf(self, *matchers):
+        return QueryBuilder(Or(*matchers))
